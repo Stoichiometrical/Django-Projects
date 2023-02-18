a9a91b72618db7612fced50924db105bd9fbbaf2
David Tendai Gondo <73646279+Stoichiometrical@users.noreply.github.com>
2023-02-18 10:38:23 +0400

    Update settings.py

--- passwordGen-project/passwordGen/settings.py
+++ passwordGen-project/passwordGen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)f51j0i%@01sv+xpqswd01jv+7!v7b4k@*=qbgb)&1^t^4ly$8'
+SECRET_KEY = '[SECRET KEY HERE]'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
